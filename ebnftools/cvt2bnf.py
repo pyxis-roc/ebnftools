93a2bd332e204fa3d9a921cf30b3e9cfbbfd93eb
sree <sree@buckyball>
2020-06-06 23:07:43 -0400

    Fix bnf generation

--- ebnftools/cvt2bnf.py
+++ ebnftools/cvt2bnf.py
@@ -7,6 +7,7 @@ from ebnftools.convert.tokens import TokenRegistry, TknLiteral, TknRegExp, TknCh
 import argparse
 import copy
 import pathlib
+import sys
 
 def sanity_check(orig, new, paranoid = True):
     orig_as_dict = dict([(r.lhs.value, r.rhs) for r in orig])
@@ -14,7 +15,7 @@ def sanity_check(orig, new, paranoid = True):
 
     for k in orig_as_dict:
         if k not in new_as_dict:
-            print(f"ERROR: {k} is *not* in bnf")
+            print(f"ERROR: {k} is *not* in bnf", file=sys.stderr)
         else:
             if isfinite(orig_as_dict, orig_as_dict[k]) and isfinite(new_as_dict, new_as_dict[k]):
                 # check for weak-equivalence by counting
@@ -23,20 +24,20 @@ def sanity_check(orig, new, paranoid = True):
                 co = count(orig_as_dict, orig_as_dict[k])
                 cn = count(new_as_dict, new_as_dict[k])
                 if co != cn:
-                    print(f"ERROR: Count for rule {k}: EBNF count {co} != BNF count {cn}")
+                    print(f"ERROR: Count for rule {k}: EBNF count {co} != BNF count {cn}", file=sys.stderr)
                 else:
-                    print(f"INFO: {k} is okay [{cn} == {co}]")
+                    print(f"INFO: {k} is okay [{cn} == {co}]", file=sys.stderr)
 
                 if paranoid:
                     s1 = set(generate2(orig_as_dict, orig_as_dict[k]))
                     s2 = set(generate2(new_as_dict, new_as_dict[k]))
 
                     if s1 != s2:
-                        print(f"ERROR: Generated strings for {k} don't match: {s1 - s2} and {s2 - s1}")
+                        print(f"ERROR: Generated strings for {k} don't match: {s1 - s2} and {s2 - s1}", file=sys.stderr)
                     else:
-                        print(f"INFO: Generated strings for {k} match")
+                        print(f"INFO: Generated strings for {k} match", file=sys.stderr)
             else:
-                print(f"ERROR: Can't check {k} for weak equivalence, contains infinite productions")
+                print(f"ERROR: Can't check {k} for weak equivalence, contains infinite productions", file=sys.stderr)
 
 def _add_token(treg, prefix, value, k = 0):
     try_token_name = prefix + f"_{k}"
@@ -108,12 +109,15 @@ if __name__ == "__main__":
         ccrwt = CharClassRewriter()
         ccrwt.rewrite(bnf, treg.v2n)
 
-        for s, t in treg.v2n.items():
+        for t, s in treg.n2v.items():
             if not isinstance(s, TknRegExp):
                 # note that s are subclasses of String/CharClass
                 print(ebnfast.Rule(ebnfast.Symbol(t), s))
             else:
-                print("Not supported regular expressions in EBNF {t} for {s}")
+                # this is usually not a problem since the token will
+                # be in the lexer anyway, but it would be nice to see
+                # if the regexp can be converted to EBNF
+                print(f"Regular expressions literals in EBNF are not supported, omitting token '{t} ::= {s}'", file=sys.stderr)
 
     for r in bnf:
         print(r)
