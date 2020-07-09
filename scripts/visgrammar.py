e8886dc3fc4431c01d7d12d02c093ad2b26d8533
sree <sree@buckyball>
2020-07-09 12:04:49 -0400

    Add script to visualize grammar

---
+++ scripts/visgrammar.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python3
+
+import argparse
+from ebnftools.ebnfast import EBNFParser, generate_dot, generate_graph
+from ebnftools.ebnfgrammar import EBNFAnnotatedGrammar
+
+if __name__ == "__main__":
+    import argparse
+    p = argparse.ArgumentParser(description="Visualize an EBNF AST as a graph")
+    p.add_argument("grammar", help="EBNF grammar")
+    p.add_argument("output_prefix", help="Output DOT file prefix")
+    args = p.parse_args()
+
+    gr = EBNFAnnotatedGrammar()
+    with open(args.grammar, "r") as f:
+        grraw = f.read()
+        gr.parse(grraw)
+
+    for r in gr.rules:
+        el = gr.ast_graph(r, True)
+        g = generate_dot(el)
+
+        fn = args.output_prefix + "_" + r.lhs.value + ".dot"
+        with open(fn, "w") as f:
+            print(fn)
+            for l in g:
+                print(l, file=f)
+
